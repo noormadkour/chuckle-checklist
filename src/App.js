822b1dacf50ee0e643d97970e5cd4308ce43de0c
Noor Madkour <noormadkour@gmail.com>
2023-08-29 14:17:04 -0500

    successful input field acceptance and post to API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,61 @@
-import "./App.css"
+import { useState, useEffect } from "react";
+import "./App.css";
+import { getAllJokes } from "./services/jokeService";
 
 export const App = () => {
-  return <div><div className="app-heading">
-    <h1>Chuckle List</h1>
-  </div></div>
-}
+  const [userInput, setUserInput] = useState("");
+
+  const submitJoke = async () => {
+    const jokeObject = {
+      text: userInput,
+      told: false,
+    };
+    
+    try {
+      const response = await fetch('http://localhost:8088/jokes', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(jokeObject)
+      });
+
+      if (response.ok) {
+        console.log('Joke submitted successfully');
+        // You can reset the userInput state here if needed
+        setUserInput('');
+      } else {
+        console.error('Failed to submit joke');
+      }
+    } catch (error) {
+      console.error('Error submitting joke:', error);
+    }
+  };
+
+
+  return (
+    <div>
+      <div className="app-heading">
+        <h1 className="app-heading-text">Chuckle List</h1>
+        <h2>Add Joke</h2>
+        <div className="joke-add-form">
+          <input
+            className="joke-input"
+            type="text"
+            value={userInput}
+            placeholder="New One Liner"
+            onChange={(event) => {
+              setUserInput(event.target.value);
+            }}
+          />
+          <button
+            className="joke-input-submit"
+            onClick={userInput ? submitJoke : null}
+          >
+            Submit Your Corny-Ass Joke
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { App } from "./App"
-import { createRoot } from "react-dom/client"
 import "./index.css"
+import { createRoot } from "react-dom/client"
+import { App } from "./App"
 
 const container = document.getElementById("root")
 const root = createRoot(container)
